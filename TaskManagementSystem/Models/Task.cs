42e74c434a3596dce4b7506d6860a02f7ad22187
shola <sholaadeniyi@gmail.com>
2020-05-23 23:23:59 -0500

    Add Task Class

---
+++ TaskManagementSystem/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystem.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Boolean IsComplete { get; set; }
+        public int CompletionPercentage { get; set; }
+        public string Note { get; set; }
+        [Required]
+        public double DailyWage { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime DateCompleted { get; set; }
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public Task()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
