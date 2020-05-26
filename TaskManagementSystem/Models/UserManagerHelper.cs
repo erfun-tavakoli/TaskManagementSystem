ede0d7b31e374d9f5de5c45efd8dccb21067eb5a
shola <sholaadeniyi@gmail.com>
2020-05-25 22:50:03 -0500

    Create role method

--- TaskManagementSystem/Models/UserManagerHelper.cs
+++ TaskManagementSystem/Models/UserManagerHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +9,26 @@ namespace TaskManagementSystem.Models
 {
     public class UserManagerHelper
     {
+        static ApplicationDbContext db = new ApplicationDbContext();
+        static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        static RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
 
+        public static void CreateRole(string roleName)
+        {
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole
+                {
+                    Name = roleName
+                }
+                );
+                db.SaveChanges();
+            }
+
+        }
     }
+
+
+
+
 }
