2047ed215b7ca928421d1dfa8ba22455ca77da62
erfun-tavak <erfuntavakoli1396@gmail.com>
2020-06-03 10:53:47 -0500

    create notification class

--- TaskManagementSystem/Models/IdentityModels.cs
+++ TaskManagementSystem/Models/IdentityModels.cs
@@ -13,12 +13,14 @@ namespace TaskManagementSystem.Models
     {
         public virtual ICollection<Project> Projects { get; set; }
         public virtual ICollection<Job> Jobs { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
         public DateTime DateJoined { get; set; }
 
         public ApplicationUser()
         {
             this.Projects = new HashSet<Project>();
             this.Jobs = new HashSet<Job>();
+            this.Notifications = new HashSet<Notification>();
             DateJoined = DateTime.Now;
         }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
@@ -34,6 +36,7 @@ namespace TaskManagementSystem.Models
     {
         public DbSet<Project>Projects { get; set; }
         public DbSet<Job>Tasks { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
         public ApplicationDbContext()
             : base("ProjectManagementDBConnectionString", throwIfV1Schema: false)
         {
---
+++ TaskManagementSystem/Models/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystem.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public Notification()
+        {
+            this.Date = DateTime.Now;
+        }
+    }
+}
