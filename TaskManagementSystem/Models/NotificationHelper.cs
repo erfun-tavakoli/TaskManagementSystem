2621daa6b3c62517edcb804c4b0dd71747f00d62
erfun-tavak <erfuntavakoli1396@gmail.com>
2020-06-03 20:32:11 -0500

    Create Methods for NotificationHelperClass

---
+++ TaskManagementSystem/Models/NotificationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementSystem.Models
+{
+    public static class NotificationHelper
+    {
+        public static void CreateJobCompletedNotification(Job job)
+        {
+            Notification notification = new Notification();
+            notification.ApplicationUser = job.Project.ApplicationUser;
+            notification.Description = "The Task " + job.Title + " has been Completed";
+        }
+
+        public static void CreateProjectCompletedNotification(Project project)
+        {
+            Notification notification = new Notification();
+            notification.ApplicationUser = project.ApplicationUser;
+            notification.Description = "The Task " + project.Title + " has been Completed";
+        }
+    }
+}
